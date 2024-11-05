11519ba8d893db045c2857171e5906139d5b25f1
Egamberdiyev-Akmal <egamberdiyevakmal2000@gmail.com>
2024-11-05 22:13:49 +0500

    What is OOP

---
+++ classes_objects.php
@@ -0,0 +1,14 @@
+<!-- 
+Sinf ob'ektlar uchun shablon, ob'ekt esa sinfning namunasidir.
+
+Faraz qilaylik, bizda Fruit nomli sinf bor. Meva nomi, rangi, vazni va boshqalar kabi 
+xususiyatlarga ega bo'lishi mumkin. Ushbu xususiyatlar qiymatlarini saqlash uchun $name, 
+$color va $weight kabi o'zgaruvchilarni belgilashimiz mumkin.
+
+Alohida ob'ektlar (olma, banan va boshqalar) yaratilganda, ular sinfdan barcha 
+xususiyatlar va xatti-harakatlarni meros qilib oladi, lekin har bir ob'ekt xususiyatlar 
+uchun turli qiymatlarga ega bo'ladi.
+
+Sinf class kalit so'z, undan keyin sinf nomi va bir juft jingalak qavs ({}) yordamida 
+aniqlanadi. Uning barcha xossalari va usullari qavs ichiga kiradi:
+-->
